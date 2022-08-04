80181824170ade9b1cdabd21b670f9c487475d95
rijeshbho <bhorijesh@gmail.com>
2022-08-04 11:10:34 +0545

    rijesh

---
+++ Rijesh-php/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>First PHP Class</title>
+</head>
+<body>
+  <?php
+echo "<h1>my heading </h1>";
+print("here <br>");
+$text="This is the text";
+echo "$text <br>";
+$a=1;
+$b=2;
+$sum=$a+$b;
+echo "the sum of $a and $b is $sum ";
+
+  ?>
+</body>
+</html>
