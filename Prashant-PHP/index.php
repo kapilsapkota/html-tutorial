56805a7ec83bfdc9c364442ee9cd551592d58ba5
itsmeinsane2265 <kasulaprashant544@gmail.com>
2022-08-08 10:13:24 +0545

    php modified

--- Prashant-PHP/index.php
+++ Prashant-PHP/index.php
@@ -90,6 +90,116 @@ for ($i=1; $i<=5 ; $i++) {
 
 
 
+//is_int() integer function checks either it is int or not
+
+$x = 531;
+$y = 65.89;
+
+echo var_dump($x);
+echo var_dump($y)."<br>";
+
+echo var_dump(is_int($x));
+echo var_dump(is_int($y));
+
+if(is_int($x)){
+	echo $x."<br>";
+
+}
+
+/*is_int() function either checks either it's int or not & returns either 
+	it's true or false */
+
+if(is_int($x))
+{
+	echo "This is integer 69 : ".$x. "<br>";
+
+}
+
+if(is_int($y))
+{
+	echo "This is integer 72 : ".$y. "<br>";
+
+}
+
+if(is_float($x))
+{
+	echo "This is float 75 : ".$x. "<br>";
+
+}
+
+
+if(is_float($y))
+{
+	echo "This is float 78 : ".$y. "<br>";
+
+}
+
+/*is_numeric() checks whether the given string is numeric or not and
+	returns either true or false*/
+
+$a = 454;
+$b = "454";
+$c = "number";	
+
+if(is_numeric($a)){
+	echo $a. " is numeric variable <br>";
+}else{
+	echo $a. " is non numeric variable <br>";
+}
+
+if(is_numeric($b)){
+	echo $b. " is numeric variable <br>";
+}else{
+	echo $b. " is non numeric variable <br>";
+}
+
+if(is_numeric($c)){
+	echo $c. " is numeric variable <br>";
+}else{
+	echo $c. " is non numeric variable <br>";
+}
+
+function checkNumberVariable($variable){
+	if(is_numeric($variable)){
+		echo $variable. " is numeric variable <br>";
+	}else{
+		echo $variable. " is non numeric variable <br>";
+	}
+}
+checkNumberVariable($a);
+checkNumberVariable($b);
+checkNumberVariable($c);
+
+//function to check float var
+$e = 55.20;
+$f = 55;
+$g = 3.2;
+function validFloat($variable){
+	if(is_float($variable)){
+		echo $variable. " is float variable <br>";
+	}else{
+		echo $variable. " is not a float variable <br>";
+	}
+}
+validFloat($e);
+validFloat($f);
+validFloat($g);
+
+
+//function to print name
+function printMyName($name=null){
+	/*if($name){
+		echo "Your name is : ".$name;
+	}else{
+		echo "No name given ";
+	}*/
+
+	$printedName = $name ? "Your Name is ".$name : " Kasula";
+	echo $printedName;
+}
+printMyName("Prashant");
+printMyName();
+	
 ?>
 
 
