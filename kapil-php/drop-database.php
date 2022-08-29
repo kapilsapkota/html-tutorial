823343689f52198faf0be4724ea2c64dabcd707b
kapilsapkota <searchin.kapil@gmail.com>
2022-08-29 12:57:48 +0545

    Added Database Connection

---
+++ kapil-php/drop-database.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SQL Connection Create Example</title>
+</head>
+<body>
+<h1>SQL Connection Create Database MySQLi Object Oriented Approach</h1>
+<?php 
+	$serverName = "localhost";
+	$userName = "root";
+	$password = "";
+
+	//Create the connection to mySql Database
+	$conn = new MySQLi($serverName,$userName,$password);
+
+	//Check connection  if true return connected 
+	if ($conn->connect_error) {
+		die("Could not connect! Connection Failed!"). $conn->connect_error;
+	}
+
+	echo "Connected Successfully!";
+
+//create a sql
+$sql = "DROP DATABASE csit_php";
+
+$query = $conn->query($sql);
+if($query === TRUE){
+	echo "Database csit_php deleted successfully!";
+}else{
+	echo "Error Deleting Database".$conn->error;
+}
+
+	//Closing the connection
+	$conn->close();
+?>
+</body>
+</html>
